5a0aa861aef1cf5003900def05fc6057a00e91ef
Light white <lightwhite520@outlook.com>
2026-08-22 16:00:47 +0800

    fix(server): load optional host environment variables

--- server/src/test/kotlin/io/openeden/server/bootstrap/HostIdentityConfigTest.kt
+++ server/src/test/kotlin/io/openeden/server/bootstrap/HostIdentityConfigTest.kt
@@ -1,6 +1,7 @@
 package io.openeden.server.bootstrap
 
 import io.ktor.server.config.MapApplicationConfig
+import io.ktor.server.config.yaml.YamlConfig
 import io.openeden.relationship.HostIdentity
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -8,6 +9,25 @@ import kotlin.test.assertFailsWith
 import kotlin.test.assertNull
 
 class HostIdentityConfigTest {
+    @Test
+    fun `application yaml maps host environment variables to an exact identity`() {
+        val previousPlatform = System.getProperty("OPENEDEN_HOST_PLATFORM")
+        val previousUserId = System.getProperty("OPENEDEN_HOST_USER_ID")
+        try {
+            System.setProperty("OPENEDEN_HOST_PLATFORM", "QQ")
+            System.setProperty("OPENEDEN_HOST_USER_ID", "owner")
+
+            val config = requireNotNull(YamlConfig(null))
+
+            assertEquals(HostIdentity("QQ", "owner"), loadHostIdentity(config))
+        } finally {
+            if (previousPlatform == null) System.clearProperty("OPENEDEN_HOST_PLATFORM")
+            else System.setProperty("OPENEDEN_HOST_PLATFORM", previousPlatform)
+            if (previousUserId == null) System.clearProperty("OPENEDEN_HOST_USER_ID")
+            else System.setProperty("OPENEDEN_HOST_USER_ID", previousUserId)
+        }
+    }
+
     @Test
     fun `missing host coordinates leave host identity unconfigured`() {
         assertNull(loadHostIdentity(MapApplicationConfig()))
